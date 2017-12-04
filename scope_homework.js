33778fab3f9dd9dab29155faf3660f0bd151e68b
Yoni Satat <yoni.satat@gmail.com>
2017-12-04 16:41:30 +0000

    answered first two questions

---
+++ scope_homework.js
@@ -0,0 +1,24 @@
+// Episode 1
+
+var name = 'Keith';
+
+var printName = function() {
+  console.log('My name is ' + name );
+};
+
+printName(); // My name is Keith
+
+// ================================================================== //
+
+//Episode 2
+
+score = 5;
+
+var result = function() {
+  var score = 3;
+  return score;
+};
+
+console.log(result()); // 3
+
+// ================================================================== //
